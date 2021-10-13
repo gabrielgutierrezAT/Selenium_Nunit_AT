6eade664945dd8496d7b9e8353059b241218d810
gabriel.gutierrez <gabriel.gutierrez@agilethought.com>
2021-10-12 20:00:53 -0500

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Selenium_Nunit_AT
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
