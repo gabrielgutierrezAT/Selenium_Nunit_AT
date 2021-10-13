9fc3e75f69a4e0048982cd7071252fc122f912d5
gabriel.gutierrez <gabriel.gutierrez@agilethought.com>
2021-10-13 17:36:50 -0500

    Adding Setup and  TearDown

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -11,23 +11,23 @@ namespace Selenium_Nunit_AT
 {
     public class Tests
     {
+        IWebDriver driver;
+
         [SetUp]
         public void Setup()
         {
-
+            driver = new ChromeDriver();
         }
 
         
         [Test]
         [Category("Search Test")]
           public void Search_text()
-          {
-              IWebDriver driver = new ChromeDriver("../../../");
+          {              
               driver.Navigate().GoToUrl("http://www.google.com");
               string search_text = System.IO.File.ReadAllText("../../../TextFile1.txt");
               driver.FindElement(By.Name("q")).SendKeys(search_text + Keys.Enter);
               Assert.That(driver.Title, Is.EqualTo(search_text + " - Buscar con Google"));
-              driver.Quit();
         }
    
         [Test]
@@ -35,14 +35,12 @@ namespace Selenium_Nunit_AT
         [Category("Regression Test")]
         public void Linkedin()
         {
-            IWebDriver driver = new ChromeDriver("../../../");
             driver.Navigate().GoToUrl("http://www.agilethought.com");
             IWebElement linkedin_btn = driver.FindElement(By.CssSelector("a[href='http://www.linkedin.com/company/agilethought']"));
             IJavaScriptExecutor jse = (IJavaScriptExecutor)driver;
             jse.ExecuteScript("arguments[0].click()", linkedin_btn);
             string title = driver.SwitchTo().Window(driver.WindowHandles[1]).Title;
             Assert.That(title, Does.Contain("linkedin").IgnoreCase);
-            driver.Quit();
         }
 
         [Test]
@@ -50,14 +48,12 @@ namespace Selenium_Nunit_AT
         [Category("Regression Test")]
         public void Facebook()
         {
-            IWebDriver driver = new ChromeDriver("../../../");
             driver.Navigate().GoToUrl("http://www.agilethought.com");
             IWebElement facebook_btn = driver.FindElement(By.CssSelector("a[href='https://www.facebook.com/AgileThought']"));
             IJavaScriptExecutor jse = (IJavaScriptExecutor)driver;
             jse.ExecuteScript("arguments[0].click()", facebook_btn);
             string title = driver.SwitchTo().Window(driver.WindowHandles[1]).Title;
             Assert.That(title, Does.Contain("facebook").IgnoreCase);
-            driver.Quit();
         }
 
         [Test]
@@ -65,14 +61,12 @@ namespace Selenium_Nunit_AT
         [Category("Regression Test")]
         public void Twitter()
         {
-            IWebDriver driver = new ChromeDriver("../../../");
             driver.Navigate().GoToUrl("http://www.agilethought.com");
             IWebElement twitter_btn = driver.FindElement(By.CssSelector("a[href='https://twitter.com/AgileThought']"));
             IJavaScriptExecutor jse = (IJavaScriptExecutor)driver;
             jse.ExecuteScript("arguments[0].click()", twitter_btn);
             string title = driver.SwitchTo().Window(driver.WindowHandles[1]).Title;
             Assert.That(title, Does.Contain("twitter").IgnoreCase);
-            driver.Quit();
         }
 
         [Test]
@@ -80,14 +74,12 @@ namespace Selenium_Nunit_AT
         [Category("Regression Test")]
         public void Instagram()
         {
-            IWebDriver driver = new ChromeDriver("../../../");
             driver.Navigate().GoToUrl("http://www.agilethought.com");
             IWebElement instagram_btn = driver.FindElement(By.CssSelector("a[href='https://www.instagram.com/agilethought/']"));
             IJavaScriptExecutor jse = (IJavaScriptExecutor)driver;
             jse.ExecuteScript("arguments[0].click()", instagram_btn);
             string title = driver.SwitchTo().Window(driver.WindowHandles[1]).Title;
             Assert.That(title, Does.Contain("instagram").IgnoreCase);
-            driver.Quit();
         }
         
 
@@ -96,18 +88,17 @@ namespace Selenium_Nunit_AT
         [Category("Regression Test")]
         public void Amazon()
         {
-            IWebDriver driver = new ChromeDriver("../../../");
             driver.Navigate().GoToUrl("http://www.amazon.com");
             string search_text = System.IO.File.ReadAllText("../../../Amazon.txt");
             driver.FindElement(By.Id("twotabsearchtextbox")).SendKeys(search_text + Keys.Enter);
             Assert.That(driver.Title, Is.EqualTo("Amazon.com : " + search_text));
-            driver.Quit();
+            
         }
 
         [TearDown]
         public void closeBrowser()
         {
-            
+            driver.Quit();
         }
 
 
